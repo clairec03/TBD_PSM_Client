da209a5331a320690fff3815f209e2bcbdcff1f7
clairec03 <clairecchen03@gmail.com>
2022-10-04 16:06:36 -0400

    Set up catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/clairechen/TBD_Podi_Personal_Space_Modeling/src
+# Build directory: /home/clairechen/TBD_Podi_Personal_Space_Modeling/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
